fed17b29898226ea8b20cf44b0f3134cdc5731e4
Amaury Bernard <amaury.bernard@epitech.eu>
2019-09-29 22:55:10 -0700

    Create the shop menu with categories

---
+++ BaseballGame5A/Assets/ShopCanvas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class ShopCanvas : MonoBehaviour
+{
+    public String categorieName = null;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void SwitchToCategory()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
