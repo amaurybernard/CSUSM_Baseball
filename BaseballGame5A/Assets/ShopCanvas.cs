cd2b4cf48f53d16a5b90f9be4915a8bcb2fbcb20
Amaury Bernard <amaury.bernard@epitech.eu>
2019-10-17 16:01:21 -0700

    ShopMenu working!

--- BaseballGame5A/Assets/ShopCanvas.cs
+++ BaseballGame5A/Assets/ShopCanvas.cs
@@ -2,11 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ShopCanvas : MonoBehaviour
 {
-    public String categorieName = null;
+    public String categoryName = null;
+    public GameObject categoriesMenu;
+    public List<GameObject> CategoryItemsMenu;
+    public GameObject ActiveMenu;
     
     
     // Start is called before the first frame update
@@ -15,9 +20,21 @@ public class ShopCanvas : MonoBehaviour
         
     }
 
-    void SwitchToCategory()
+    public void PreviousAction()
     {
-        
+        if (categoryName == null) {
+            SceneManager.LoadScene("SampleScene");
+        } else {
+            ActiveMenu.SetActive(false);
+            categoriesMenu.SetActive(true);
+        }
+    }
+    
+    public void switchToMenu(GameObject menu)
+    {
+        ActiveMenu.SetActive(false);
+        menu.SetActive(true);
+        ActiveMenu = menu;
     }
     // Update is called once per frame
     void Update()
